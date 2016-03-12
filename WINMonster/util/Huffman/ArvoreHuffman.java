36c015407d5666daa0fbef52ff3acc8e9b25c09a
leonMMS <leon_darth@hotmail.com>
2016-03-11 22:49:17 -0300

    Modificações no Util

---
+++ WINMonster/util/Huffman/ArvoreHuffman.java
@@ -0,0 +1,16 @@
+package util.Huffman;
+
+public class ArvoreHuffman {
+
+	private int frequencia;
+
+	public int getFrequencia() {
+		return frequencia;
+	}
+
+	public void setFrequencia(int frequencia) {
+		this.frequencia = frequencia;
+	}
+	
+	
+}
---
+++ WINMonster/util/Huffman/FolhaHuffman.java
@@ -0,0 +1,15 @@
+package util.Huffman;
+
+public class FolhaHuffman extends ArvoreHuffman{
+
+	private char info;
+
+	public char getInfo() {
+		return info;
+	}
+
+	public void setInfo(char info) {
+		this.info = info;
+	}
+	
+}
