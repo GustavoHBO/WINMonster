5909794bd7cc5d13234854fec5643cb809c38151
MrGustavo <gustavo.oliveira.fsa@gmail.com>
2016-03-15 16:03:22 -0300

    Implementação de métodos no controller
    Implementação do método de leitura.
    Implementação do método de escrita.
    E devidos testes.

--- WINMonster/controller/Controller.java
+++ WINMonster/controller/Controller.java
@@ -3,6 +3,14 @@ package controller;
 
 
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 import util.FilaPrioridade;
 import util.Huffman.ArvoreHuffman;
 import util.Huffman.FolhaHuffman;
@@ -16,57 +24,111 @@ import util.Huffman.FolhaHuffman;
  *
  */
 public class Controller {
-/*---------------------------------------------------------------------------------*/
+	/*---------------------------------------------------------------------------------*/
 	private static final int NUM = 256;
 	private static Controller instance = new Controller();
-	
+
 	private Controller(){}
-	
+
 	public static Controller getInstance(){
 		if(instance == null)
 			instance = new Controller();
-		
+
 		return instance;
 	}
-	
+
 	public static void zerarSingleton(){
 		instance = new Controller();
 	}
-/*---------------------------------------------------------------------------------*/
-	
+	/*---------------------------------------------------------------------------------*/
+
 	public static int[] calcularFrequencia(String texto){
 		int[] frequencias = new int[NUM];
 		char[] caracteres = texto.toCharArray();
-		
+
 		for (int i = 0; i < caracteres.length; i++)
-            frequencias[caracteres[i]]++;
-		
+			frequencias[caracteres[i]]++;
+
 		return frequencias;
 	}
-/*---------------------------------------------------------------------------------*/
-	
+	/*---------------------------------------------------------------------------------*/
+
 	public static FilaPrioridade criarFilaComFrequencias(int[] frequencias){
 		FilaPrioridade fila = new FilaPrioridade();
-		
+
 		for(int i = 0; i < frequencias.length; i++){
 			if(frequencias[i] > 0){
 				FolhaHuffman folha = new FolhaHuffman();
 				folha.setFrequencia(frequencias[i]);
 				folha.setInfo((char)i);
-				
+
 				fila.inserir(frequencias[i], folha);
 			}
 		}
 		return fila;
 	}
-/*---------------------------------------------------------------------------------*/
-	
+	/*---------------------------------------------------------------------------------*/
+
 	public static String[] gerarCodigoHuffman(ArvoreHuffman arvore){
 		String[] dicionario = new String[NUM];
-		
+
 		ArvoreHuffman.construirCodigo(dicionario, arvore, "");
-		
+
 		return dicionario;
 	}
-	
+
+
+	/**
+	 * Método responsável pela leitura do arquivo e retorna um array dos dados lidos.
+	 * @param arquivo - Nome do arquivo a ser lido.
+	 * @return dados - Array dos caracteres lidos no arquivo.
+	 */
+
+	public String lerArquivo(String arquivo){
+
+		String dados = new String();// String onde será armazenada as informações lidas.
+		FileReader arq = null;// Instância do arquivo.
+		BufferedReader buffer = null;// Instância do leitor do arquivo.
+		try {// Ver como será tratado esse erro.
+			arq = new FileReader(arquivo);
+			buffer = new BufferedReader(arq);
+
+			while (buffer.ready()){//Irá ser valido até encontrar o fim do arquivo.
+				dados = dados + buffer.readLine();// Lê linha por linha no arquivo e concatena no final da string dados.
+			}
+			buffer.close();// Finalizo o leitor do arquivo.
+			arq.close();// Finalizo o arquivo.
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return dados;
+	}
+
+	/**
+	 * Método responsável pela escrita dos arquivos seguindo o algoritmo de Huffman.
+	 * @param arrayCaractere - Array com os códigos dos caracteres.
+	 * @param nomeArquivo - Nome do arquivo a ser gravado.
+	 * @param caminhoArquivo - Caminho ao qual o arquivo será armazenado.
+	 */
+	public void escreverArquivo(char[] arrayCaractere, String nomeArquivo, String caminhoArquivo){
+
+		FileWriter fileWrite = null;
+		BufferedWriter buffWrite = null;
+		String nomeCaminho = caminhoArquivo + nomeArquivo + ".monster";// Aqui é especificado o caminho e o nome do arquivo.
+		File arquivo = new File(nomeCaminho);//Instância do arquivo.
+		try {// Ver como vai ser tratado esse tipo de erro.
+			arquivo.createNewFile();//Crio o arquivo no diretório escolhido.
+			fileWrite = new FileWriter(arquivo);// Defino o arquivo ao qual irá ser escrito.
+			buffWrite = new BufferedWriter(fileWrite);// Defino como irá escrever.
+			
+			buffWrite.write(arrayCaractere);// Escreve o conteúdo da array no arquivo.
+
+			buffWrite.close();// Fecho o BufferedWrite.
+			fileWrite.close();// Fecho o FileWrite.
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
---
+++ WINMonster/teste/LerArquivoTest.java
@@ -0,0 +1,53 @@
+package teste;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import controller.Controller;
+
+public class LerArquivoTest {
+
+	private Controller controller = null;
+	@Before
+	public void setUp() throws Exception {
+		Controller.zerarSingleton();
+		controller = Controller.getInstance();
+
+	}
+
+	@Test
+	public void LerArquivoSucesso() {
+
+		File file = new File("arquivo.txt");
+		FileWriter fileWrite = null;
+		BufferedWriter buffer = null;
+		try {
+			file.createNewFile();
+			fileWrite = new FileWriter(file);
+			buffer = new BufferedWriter(fileWrite);
+			
+			buffer.write("ola mundo");
+			
+			file.delete();
+			buffer.close();
+			fileWrite.close();
+			
+		} catch (IOException e) {
+			fail();
+		}
+		
+		String string = controller.lerArquivo("arquivo.txt");
+		String frase = "ola mundo";
+		assertEquals(frase, string);
+
+	}
+
+}
